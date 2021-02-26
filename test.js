a39c50b29813a3b86b94495dfb9b385767bca280
ms-musfiq <msmusfiq72@gmail.com>
2021-02-26 22:54:31 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+
+
+
+try{
+    alert("hi every one");  
+    alert(v);
+    alert("by by");
+    alert("good bye");
+}
+catch(error){
+    console.log(error sd);    /*syntext error here*/
+}
+finally{
+    alert("good bye Every one");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("hi every one");
+// alert(v);
+// alert("by by");
+// alert("good bye");
